48557fae46d3a72b39830ed7c827bd13975ea03e
Selenuix <selenuix.temp@gmail.com>
2020-11-22 21:53:03 +0100

    Deploy dev

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,6 @@ module.exports = {
         ['meta', { name: 'theme-color', content: '#3eaf7c' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
         ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['meta', { name: 'viewport', content: 'black' }],
         ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
         ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
         ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-msapplication-icon-144x144.png' }],
